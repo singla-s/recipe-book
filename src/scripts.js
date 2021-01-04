909cb73c0c19bc4a2fdd46c4e7dcd996deea4322
sinnisingla@outlook.com <sinnisingla@outlook.com>
2021-01-04 16:47:22 +0530

    Add dropdown to navbar

---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+function toggleDropdownDisplay() {
+    var x = document.querySelector(".sub-menu");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}

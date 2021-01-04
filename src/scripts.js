8f30857361f91fc5e5fd1c775c8c08a5c880021a
sinnisingla@outlook.com <sinnisingla@outlook.com>
2021-01-05 00:02:05 +0530

    Add Recipe Detail Component

--- src/scripts.js
+++ src/scripts.js
@@ -6,3 +6,12 @@ function toggleDropdownDisplay() {
       x.style.display = "none";
     }
 }
+
+function toggleDropdownRecipeDetail() {
+  var x = document.querySelector(".dropdown-menu-recipe-details");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}

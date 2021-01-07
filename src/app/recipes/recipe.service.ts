96cfd4e499c29f77c2b20a5f6152dd23f5fcdb66
sinnisingla@outlook.com <sinnisingla@outlook.com>
2021-01-07 23:39:18 +0530

    return replica of recipe array not the reference

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Recipe } from '../recipes/recipe.model'
 
 export class RecipeService {
-    recipes: Recipe[] = [];
+    private recipes: Recipe[] = [];
 
     constructor() {
         this.recipes.push(new Recipe("Banana Shake", "Banana Shake description", "https://thecozycook.com/wp-content/uploads/2018/06/Peanut-Butter-Banana-Smoothie.jpg"));
         this.recipes.push(new Recipe("Pav Bhaji", "Pav Bhaji description", "https://media.istockphoto.com/photos/indian-mumbai-food-pav-bhaji-from-vegetables-with-bread-closeup-in-a-picture-id1057140522?k=6&m=1057140522&s=612x612&w=0&h=JqbUoiQ7usEWOdsuaBqcqbRkDuIYXCg6qbyhsid8cmo="));
+    }
+
+    getRecipes() {
+        return this.recipes.slice();
     }
 }

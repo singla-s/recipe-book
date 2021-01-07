6baaa8dbdcdef2933dd00afb628745199e62b26c
sinnisingla@outlook.com <sinnisingla@outlook.com>
2021-01-07 23:26:39 +0530

    Add Recipe Service to Recipe Component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service'
 
 @Component({
   selector: 'app-recipes-list',
@@ -9,12 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent implements OnInit {
   @Output() recipeDetail = new EventEmitter<Recipe>(); 
   recipes: Recipe[] = [];
-  constructor() { 
-    this.recipes.push(new Recipe("Banana Shake", "Banana Shake description", "https://thecozycook.com/wp-content/uploads/2018/06/Peanut-Butter-Banana-Smoothie.jpg"));
-    this.recipes.push(new Recipe("Pav Bhaji", "Pav Bhaji description", "https://media.istockphoto.com/photos/indian-mumbai-food-pav-bhaji-from-vegetables-with-bread-closeup-in-a-picture-id1057140522?k=6&m=1057140522&s=612x612&w=0&h=JqbUoiQ7usEWOdsuaBqcqbRkDuIYXCg6qbyhsid8cmo="));
+  constructor(private recipeService: RecipeService) { 
+    
   }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.recipes;
   }
 
   showRecipeDetail(recipe: Recipe) {
